5e3a348a330960afc90481dd875faf61563deb2a
shizhiyoubajiu <1458842209@qq.com>
2019-07-03 08:42:49 +0800

    shop

---
+++ two/day2/shop/src/components/NavBread.vue
@@ -0,0 +1,17 @@
+<template>
+ <div class="NavBread">
+ <nav class="nav-breadcrumb">
+       <!-- 面包屑的组件 使用slot占口子 -->
+      <slot name="bread"></slot>
+</nav>
+ </div>
+</template>
+ 
+<script>
+export default {
+}
+</script>
+ 
+<style scoped >
+ 
+</style>
